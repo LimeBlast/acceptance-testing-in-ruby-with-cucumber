b9f36a4e1e8a8a62d419882e8e2fc6c7a5129920
Daniel Hollands <daniel.hollands@intercity-uk.com>
2014-12-18 15:16:50 +0000

    meh

--- features/support/config.rb
+++ features/support/config.rb
@@ -1,12 +1,10 @@
 require 'minitest/autorun'
 require 'minitest/assertions'
 require 'minitest/test'
-
 module Assertions
   attr_accessor :assertions
 end
 
 World(Minitest::Assertions)
 World(Assertions)
-
 Before { self.assertions = 0 }
