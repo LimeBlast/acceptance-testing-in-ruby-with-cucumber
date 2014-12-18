e3844296df3f0c39f23f9a090dbb86f796bea376
Daniel Hollands <daniel.hollands@intercity-uk.com>
2014-12-18 15:30:05 +0000

    Added a transform to turn all strings into ints

--- features/step_definitions/calculator_steps.rb
+++ features/step_definitions/calculator_steps.rb
@@ -2,14 +2,14 @@ Given(/^I have a calculator$/) do
   @calculator = Calculator.new
 end
 
-# Given(/^I (.+) a calculator$/) do
-#   @calculator = Calculator.new
-# end
-
 When(/^I add (\d+) and (\d+)$/) do |arg1, arg2|
   @result = @calculator.add arg1.to_i, arg2.to_i
 end
 
-Then('the result should be $expected') do |expected|
-  @result.must_equal expected.to_i
+Then(/^the result should be (\d+)$/) do |expected|
+  @result.must_equal expected
+end
+
+Transform /^(\d+)$/ do |number|
+  number.to_i
 end
